2ecaa3eac4014c9bf7668d6a63e21e288997699b
summer <yangmaosheng@summer.local>
2018-02-28 22:24:37 +0800

    Reslove isHome cann't use in Android5.0+
    1:使用UsageStatsManager进行获取当前最顶层activity，进而
    判断出当前是否在桌面。

--- FloatWindowDemo/app/src/main/java/demo/sunrise/com/floatwindowdemo/FloatWindowService.java
+++ FloatWindowDemo/app/src/main/java/demo/sunrise/com/floatwindowdemo/FloatWindowService.java
@@ -2,19 +2,29 @@ package demo.sunrise.com.floatwindowdemo;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class FloatWindowService extends Service {
+
+    private static final String TAG = "FloatWindowService";
 
     private Handler handler = new Handler();
 
@@ -84,12 +94,18 @@ public class FloatWindowService extends Service {
      * @return whether it is lanucher.
      */
     private boolean isHome() {
-        ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningTaskInfo> rti = mActivityManager.getRunningTasks(0);
+
+        // getRunningTasks在Android5.0后已被隐藏。
+        /*ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> rti = mActivityManager.getRunningTasks(1);
+        Log.d(TAG, "rti: "+rti);
         if (rti.isEmpty()){
             return true;
         }
-        return getHomes().contains(rti.get(0).topActivity.getPackageName());
+        Log.d(TAG, "topactivity: "+rti.get(0).topActivity.getPackageName());
+        return getHomes().contains(rti.get(0).topActivity.getPackageName());*/
+
+        return getHomes().contains(getTopPackage());
     }
 
     private List<String> getHomes() {
@@ -100,8 +116,53 @@ public class FloatWindowService extends Service {
         List<ResolveInfo> resolveInfos = packageManager.queryIntentActivities(intent
                 ,PackageManager.MATCH_DEFAULT_ONLY);
         for(ResolveInfo ri:resolveInfos){
-            names.add(ri.activityInfo.packageName);
+            names.add(ri.activityInfo.processName);
         }
+        Log.d(TAG, "getHomes: names:"+names.toString());
         return names;
+    }
+
+
+    static class RecentUseComparator implements Comparator<UsageStats> {
+
+        @Override
+
+        public int compare(UsageStats lhs,UsageStats rhs) {
+
+            return (lhs.getLastTimeUsed()> rhs.getLastTimeUsed()) ? -1 : (lhs
+
+                    .getLastTimeUsed()== rhs.getLastTimeUsed()) ? 0 : 1;
+
+        }
+    }
+
+    /**
+     * 使用UsageStatsManager
+     * @return
+     */
+    private String getTopPackage() {
+
+        Log.d(TAG,"===getTopPackage=");
+
+        long ts = System.currentTimeMillis();
+
+        RecentUseComparator mRecentComp = new RecentUseComparator();
+
+        UsageStatsManager mUsageStatsManager =(UsageStatsManager) getSystemService(Context.USAGE_STATS_SERVICE);
+
+        List<UsageStats> usageStats =mUsageStatsManager.queryUsageStats(
+                UsageStatsManager.INTERVAL_BEST,ts - 10000, ts);  //查询ts-10000 到ts这段时间内的UsageStats，由于要设定时间限制，所以有可能获取不到
+
+        if (usageStats == null)
+            return "";
+        if (usageStats.size() == 0)
+            return "";
+
+        Collections.sort(usageStats,mRecentComp);
+
+        Log.d(TAG,"====usageStats.get(0).getPackageName()"+ usageStats.get(0).getPackageName()+" size:"+usageStats.size());
+
+        return usageStats.get(0).getPackageName();
+
     }
 }
